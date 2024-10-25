8e095032ba70cdd2e59ca9429592e45d1f5df90a
jjhim0531 <jjhim531@naver.com>
2024-10-25 17:56:04 +0900

    241025 kh

---
+++ 10_React/my-gpt/src/components/ChatDisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatDisplay = ({ chatDataList }) => {
+    return (
+        <div>{chatDataList.map((chat, index) =>
+            <div key>
+
+            </div>
+        )}</div>
+    )
+}
+
+export default ChatDisplay
